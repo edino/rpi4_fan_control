3112665f8a1d1a629cf9ae35aeb42d8f305d9a09
Edino de Souza <eddypqd@gmail.com>
2024-02-24 19:51:40 -0500

    Update rpi4_fan_control.sh

--- rpi4_fan_control.sh
+++ rpi4_fan_control.sh
@@ -7,6 +7,11 @@
 # License: GPL-3.0 license - https://github.com/edino/rpi4_fan_control/tree/main?tab=GPL-3.0-1-ov-file
 ##############################################################################
 
+
+# The FAN tested with this script is the one listed below:
+
+# [GeeekPi Aluminum Heatsink with PWM Controllable Fan for Raspberry Pi 4, Pi 4 Armor Lite Heatsink with PWM Speed Control Fan for Raspberry Pi 4 Model B](https://www.amazon.ca/GeeekPi-Raspberry-Aluminum-Heatsink-Controllable/dp/B091L1XKL6)
+
 # Script Summary: Control fan speed based on CPU temperature using GPIO pin. Temp range 30-70°C, adjusting PWM values.
 
 # Purpose: The purpose of the script is to control the fan speed of a Raspberry Pi 4 based on the CPU temperature. It defines temperature thresholds and adjusts the PWM values to set the fan speed accordingly. The script continuously monitors the CPU temperature and adjusts the fan speed to maintain it within the specified range, aiming to provide effective cooling while minimizing noise.
