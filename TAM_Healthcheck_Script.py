fc996ab61813ad6e479dee2e1765a794870d925c
Edino de Souza <eddypqd@gmail.com>
2024-04-24 17:41:11 -0400

    Update TAM_Healthcheck_Script.py

--- TAM_Healthcheck_Script.py
+++ TAM_Healthcheck_Script.py
@@ -5,7 +5,7 @@ import time
 
 # Function to log commands
 def log_command(command, description, log_file):
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S:%Z')
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S_%Z')
     with open(log_file, 'a') as f:
         f.write(f"[{timestamp}] {description}\n")
         f.write(f"[{timestamp}] Running: {command}\n")
@@ -19,7 +19,7 @@ def main():
     nvram_output = subprocess.run(["nvram", "get", "#li.serial"], capture_output=True, text=True).stdout.strip()
 
     # Log file path
-    log_file = f"/var/tam_healthcheck_{nvram_output}-{datetime.now().strftime('%Y-%m-%d_at_%H:%M:%S:%Z')}.log"
+    log_file = f"/var/tam_healthcheck_{nvram_output}-{datetime.now().strftime('%Y-%m-%d_at_%H:%M:%S_%Z')}.log"
     print(f"Executing commands and saving output to {log_file} ...")
 
     log_command("date", "Display current date and time", log_file)
