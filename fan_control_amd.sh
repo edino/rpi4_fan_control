b352eeb7c7103675b75fbb5b90a83fd8568e5ed6
Edino de Souza <eddypqd@gmail.com>
2024-04-30 10:41:33 -0400

    Update fan_control_amd.sh

--- fan_control_amd.sh
+++ fan_control_amd.sh
@@ -3,9 +3,9 @@
 # Temperature thresholds
 MIN_TEMP=30
 MAX_TEMP=70
-TEMP_RANGE=$(expr $MAX_TEMP - $MIN_TEMP)
-TEMP_STEP=$(expr $TEMP_RANGE \* 10 / 255)
-if [ $TEMP_STEP -eq 0 ]; then
+TEMP_RANGE=$((MAX_TEMP - MIN_TEMP))
+TEMP_STEP=$((TEMP_RANGE * 10 / 255))
+if [ "$TEMP_STEP" -eq 0 ]; then
     TEMP_STEP=1
 fi
 echo "MIN_TEMP: $MIN_TEMP, MAX_TEMP: $MAX_TEMP, TEMP_STEP: $TEMP_STEP"
@@ -25,7 +25,7 @@ map_temp_to_pwm() {
     if [ "$temp" -lt "$MIN_TEMP" ]; then
         echo 0
     else
-        local index=$(( ($temp - $MIN_TEMP) * 10 / $TEMP_STEP ))
+        local index=$(( (temp - MIN_TEMP) * 10 / TEMP_STEP ))
         if [ "$index" -lt 0 ]; then
             index=0
         elif [ "$index" -ge 255 ]; then
@@ -54,7 +54,7 @@ get_cpu_temp() {
 }
 
 # Function to get NPU temperature
-get_npu_temp() {    
+get_npu_temp() {
     npu_temp=$(grep 'NPU_CPU_Temperature : ' /sdisk/tslog/xgs-healthmond.log | tail -n 1 | sed 's/.*: +//;s/ Degrees.*//')
     echo $npu_temp
     echo "$(date) - NPU Temperature: $npu_temp ºC" >> $LOG_FILE
@@ -65,7 +65,7 @@ convert_pwm_to_rpm() {
     pwm=$1
     known_pwm=255
     known_rpm=5000
-    rpm=$(expr \( $pwm \* $known_rpm + 0.5 \) / $known_pwm)
+    rpm=$(( (pwm * known_rpm + 0.5) / known_pwm ))
     echo $rpm
 }
 
@@ -83,7 +83,7 @@ fi
 while true; do
     cpu_temp=$(get_cpu_temp)
     npu_temp=$(get_npu_temp)
-    max_temp=$(expr $cpu_temp \> $npu_temp ? $cpu_temp : $npu_temp)  # Use the higher of the two temperatures
+    max_temp=$(( cpu_temp > npu_temp ? cpu_temp : npu_temp ))  # Use the higher of the two temperatures
     target_speed=$(map_temp_to_pwm $max_temp)
     rpm=$(convert_pwm_to_rpm $target_speed)
     set_fan_speed $target_speed
