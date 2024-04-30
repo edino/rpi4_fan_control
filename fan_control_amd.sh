8b44b7804aae9a6b8fffe4305655821e4d65a994
Edino de Souza <eddypqd@gmail.com>
2024-04-30 10:20:52 -0400

    Update fan_control_amd.sh

--- fan_control_amd.sh
+++ fan_control_amd.sh
@@ -3,16 +3,17 @@
 # Temperature thresholds
 MIN_TEMP=30
 MAX_TEMP=70
-TEMP_STEP=$((($MAX_TEMP - $MIN_TEMP) * 10 / 255))
-if ((TEMP_STEP == 0)); then
+TEMP_RANGE=$(expr $MAX_TEMP - $MIN_TEMP)
+TEMP_STEP=$(expr $TEMP_RANGE \* 10 / 255)
+if [ $TEMP_STEP -eq 0 ]; then
     TEMP_STEP=1
 fi
 echo "MIN_TEMP: $MIN_TEMP, MAX_TEMP: $MAX_TEMP, TEMP_STEP: $TEMP_STEP"
 
 # Define PWM_VALUES array
-PWM_VALUES=()
+PWM_VALUES=""
 for i in $(seq 0 255); do
-    PWM_VALUES[$i]=$i
+    PWM_VALUES="$PWM_VALUES $i"
 done
 
 # Log file
@@ -20,23 +21,23 @@ LOG_FILE="/sdisk/tslog/fan_control.log"
 
 # Function to map temperature to fan speed PWM value
 map_temp_to_pwm() {
-    local temp=$1
-    if ((temp < MIN_TEMP)); then
+    temp=$1
+    if [ $temp -lt $MIN_TEMP ]; then
         echo 0
     else
-        local index=$(awk -v temp=$temp -v min=$MIN_TEMP -v step=$TEMP_STEP 'BEGIN { printf "%.0f", (temp - min) * 10 / step }')
-        if ((index < 0)); then
+        index=$(expr \( $temp - $MIN_TEMP \) \* 10 / $TEMP_STEP)
+        if [ $index -lt 0 ]; then
             index=0
-        elif ((index >= 255)); then
+        elif [ $index -ge 255 ]; then
             index=255
         fi
-        echo ${PWM_VALUES[$index]}
+        echo $PWM_VALUES | cut -d ' ' -f $(expr $index + 1)
     fi
 }
 
 # Function to set fan speed
 set_fan_speed() {
-    local speed=$1
+    speed=$1
     echo $speed > /sys/devices/platform/nct6775.2608/hwmon/hwmon0/pwm2
     if [ $? -ne 0 ]; then
         echo "$(date) - Failed to set fan speed to $speed (PWM)" >> $LOG_FILE
@@ -47,24 +48,24 @@ set_fan_speed() {
 
 # Function to get CPU temperature
 get_cpu_temp() {
-    local cpu_temp=$(grep -oP '(?<=Host_CPU_Temperature : \+)[0-9]+(\.[0-9]+)?' /sdisk/tslog/xgs-healthmond.log | tail -n 1)
+    cpu_temp=$(grep -oP '(?<=Host_CPU_Temperature : \+)[0-9]+(\.[0-9]+)?' /sdisk/tslog/xgs-healthmond.log | tail -n 1)
     echo $cpu_temp
     echo "$(date) - CPU Temperature: $cpu_temp ºC" >> $LOG_FILE
 }
 
 # Function to get NPU temperature
-get_npu_temp() {    
-    local npu_temp=$(grep -oP '(?<=NPU_CPU_Temperature : \+)[0-9]+(\.[0-9]+)?' /sdisk/tslog/xgs-healthmond.log | tail -n 1)
+get_npu_temp() {
+    npu_temp=$(grep -oP '(?<=NPU_CPU_Temperature : \+)[0-9]+(\.[0-9]+)?' /sdisk/tslog/xgs-healthmond.log | tail -n 1)
     echo $npu_temp
     echo "$(date) - NPU Temperature: $npu_temp ºC" >> $LOG_FILE
 }
 
 # Function to convert PWM to RPM
 convert_pwm_to_rpm() {
-    local pwm=$1
-    local known_pwm=255
-    local known_rpm=5000
-    local rpm=$(echo "scale=0; ($pwm * $known_rpm + 0.5) / $known_pwm" | bc)
+    pwm=$1
+    known_pwm=255
+    known_rpm=5000
+    rpm=$(expr \( $pwm \* $known_rpm + 0.5 \) / $known_pwm)
     echo $rpm
 }
 
@@ -82,7 +83,7 @@ fi
 while true; do
     cpu_temp=$(get_cpu_temp)
     npu_temp=$(get_npu_temp)
-    max_temp=$((cpu_temp > npu_temp ? cpu_temp : npu_temp))  # Use the higher of the two temperatures
+    max_temp=$(expr $cpu_temp \> $npu_temp ? $cpu_temp : $npu_temp)  # Use the higher of the two temperatures
     target_speed=$(map_temp_to_pwm $max_temp)
     rpm=$(convert_pwm_to_rpm $target_speed)
     set_fan_speed $target_speed
